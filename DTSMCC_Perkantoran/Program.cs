f8389d1c9e11b77bd2a8556a9845325b1c08eda6
adittiyaasril <adittiyaasril369@gmail.com>
2022-09-16 19:28:53 +0700

    Add project files.

---
+++ DTSMCC_Perkantoran/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DTSMCC_Perkantoran
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama Karyawan : ");
+
+            //Looping Pegawai
+            string[] NamaPegawai = { "Agus", "Susanti", "Patrick", "Mukidi" };
+            foreach (string i in NamaPegawai)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Decision Absensi Pegawai
+            Console.WriteLine("\nAbsensi Hari Ini : ");
+            TimeSpan JamKerja = TimeSpan.Parse("07:00");
+            TimeSpan Jam = TimeSpan.Parse("08:00");
+
+            if (Jam <= JamKerja)
+            {
+                Console.WriteLine("Selamat Pagi");
+            }
+            else
+            {
+                Console.WriteLine("Anda Terlambat");
+
+            }
+            Console.WriteLine("\nData Pegawai : ");
+            DataPegawai("Agus", "CEO");
+            Console.Write("Gaji Anda Bulan Ini : ");
+            Console.WriteLine(GajiPegawai(20000000, 1500000));
+        }
+        //Method Data Pegawai
+        static void DataPegawai(string Nama, string Jabatan)
+        {
+            Console.WriteLine(Nama + " Jabatan " + Jabatan);
+        }
+
+        static int GajiPegawai(int Gaji, int Tunjangan)
+        {
+            return Gaji + Tunjangan;
+        }
+    }
+
+
+}
